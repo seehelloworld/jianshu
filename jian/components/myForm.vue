a222396c37246aa4f7c0b1b0e7971f38632d473e
fishAnddog <791461818@qq.com>
2018-04-27 23:21:09 +0800

    j

---
+++ jian/components/myForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <form class="new-comment" method="post">
+            <slot></slot>
+            <textarea name="comment-content" placeholder="写下你的评论" :value="value"></textarea>
+            <div class="write-function-block">
+                <div class="emoji-modal-wrap">
+                    <a class="emoji "  @click="show=!show">
+                        <i class="fa fa-smile-o"></i>
+                    </a>
+                    <emoji :class="show==true?'emoji-ji':''"  @select="selectEmoji"></emoji>
+                    <div class="emoji-modal"></div>
+                </div>
+                <div class="hint">Ctrl+Enter发送</div>
+                <a href="#" class="btn-send">发送</a>
+                <a href="#" class="btn-cancel">取消</a>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+    import emoji from './emoji';
+    export default {
+        name:"myForm",
+        components:{
+            emoji,     
+        }
+        ,
+        data () {
+            return {
+                 show:true,
+                   value: '',
+                   showEmoji: false,
+                   data: []
+            }
+        },
+        methods:{
+                selectEmoji (code) {
+                this.showEmoji = false
+                this.value += code
+    },
+        }
+    }
+</script>
+<style scoped>
+
+    .emoji-ji{
+        display:none;
+    }
+</style>
---
+++ jian/components/pup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pup">
+      <div class="pup-top">分享到微信</div>
+      <div class="pup-up"></div>
+  </div>
+</template>
+<script>
+    export default {
+        name:'pup',
+        data(){
+
+        }
+    }
+
+</script>
+<style scoped>
+.pup-top{
+    background-color: gray;
+    color: white;
+    height: 40px;
+    padding-top: 10px;
+    border-radius: 5px 5px;
+    white-space: nowrap;
+}
+.pup-up{
+        width: 0;
+        height:0; 
+        border-left:8px solid transparent; 
+        border-right:8px solid transparent; 
+        border-bottom:16px solid gray;
+        transform: rotate(180deg);
+        position: relative;
+        top: -3px;
+        left: 3px;
+}
+
+
+</style>
